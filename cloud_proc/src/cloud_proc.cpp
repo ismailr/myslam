4d6bed46c9daffd6c88767de1762a96115c0f87e
Ismail <ismail@tass.telkomuniversity.ac.id>
2016-12-19 16:21:40 +0700

    1. Moving visualization marker to layout_prediction 2. Preparing data association

--- cloud_proc/src/cloud_proc.cpp
+++ cloud_proc/src/cloud_proc.cpp
@@ -1,26 +1,42 @@
 #include <ros/ros.h>
 #include <iostream>
-
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Point.h>
-#include <visualization_msgs/Marker.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-
 #include <pcl/ModelCoefficients.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/common/distances.h>
-
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+pcl::PointCloud<pcl::PointXYZ> filter_cloud(pcl::PointCloud<pcl::PointXYZ>);
+void generate_lines_ransac(pcl::PointCloud<pcl::PointXYZ>);
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr&);
 
 ros::Publisher pub_filtered_cloud;
-ros::Publisher pub_marker;
 ros::Publisher pub_line_segment;
+
+int
+main (int argc, char** argv)
+{
+	pcl::console::setVerbosityLevel(pcl::console::L_ALWAYS);
+
+	ros::init(argc,argv,"cloud_proc");
+	ros::NodeHandle nh;
+
+	ros::Subscriber sub_point = nh.subscribe ("pointcloud", 1, cloud_cb);
+
+	pub_filtered_cloud = nh.advertise<sensor_msgs::PointCloud2> ("filtered_pointcloud",1);
+	pub_line_segment = nh.advertise<std_msgs::Float64MultiArray> ("line_segment",1);
+
+	ros::spin();
+
+	return 0;
+}
 
 pcl::PointCloud<pcl::PointXYZ> 
 filter_cloud(pcl::PointCloud<pcl::PointXYZ> cloud)
@@ -38,20 +54,8 @@ filter_cloud(pcl::PointCloud<pcl::PointXYZ> cloud)
 
 void generate_lines_ransac(pcl::PointCloud<pcl::PointXYZ> cloud)
 {
-	visualization_msgs::Marker marker;
-	marker.header.frame_id = "base_link";
-	marker.header.stamp = ros::Time::now();
 
-	marker.id = 0;
-	marker.type = visualization_msgs::Marker::LINE_LIST;
-
-	marker.action = visualization_msgs::Marker::ADD;
-	marker.scale.x = 0.1;
-	marker.color.a = 1.0;
-	marker.color.r = 0.0;
-	marker.color.g = 1.0;
-	marker.color.b = 1.0; 
-
+	std_msgs::Float64MultiArray points_msg;
 	std::vector<pcl::PointIndices> cluster_indices;
 
 	// NaN-based clustering
@@ -102,6 +106,8 @@ void generate_lines_ransac(pcl::PointCloud<pcl::PointXYZ> cloud)
 		}
 	}
 
+	
+	// For every cluster j ...
 	int j = 0;
 	for(std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin();
 			it != cluster_indices.end();
@@ -116,10 +122,9 @@ void generate_lines_ransac(pcl::PointCloud<pcl::PointXYZ> cloud)
 		cloud_cluster->height = 1;
 		cloud_cluster->is_dense = true;
 
-		// Do sequential multi RANSAC
+		// ... do sequential multi RANSAC
 		while(true)
 		{
-			std_msgs::Float64MultiArray line_segment;
 
 			pcl::ModelCoefficients coefficients;
 			pcl::PointIndices inliers;
@@ -151,28 +156,23 @@ void generate_lines_ransac(pcl::PointCloud<pcl::PointXYZ> cloud)
 				q.y = - cloud_cluster->points[inliers.indices.back()].x;
 				q.z = 0;
 
-				line_segment.data.clear();
-				line_segment.data.push_back(p.x);
-				line_segment.data.push_back(p.y);
-//				line_segment.data.push_back(p.z);
-				line_segment.data.push_back(q.x);
-				line_segment.data.push_back(q.y);
-//				line_segment.data.push_back(q.z);
+				points_msg.data.push_back(p.x);
+				points_msg.data.push_back(p.y);
+//				points_msg.data.push_back(p.z);
+				points_msg.data.push_back(q.x);
+				points_msg.data.push_back(q.y);
+//				points_msg.data.push_back(q.z);
 
-				marker.points.push_back(p);
-				marker.points.push_back(q);
 			}
 
 			pcl::PointCloud<pcl::PointXYZ>::iterator cloud_iter = cloud_cluster->begin();
 			cloud_cluster->erase(cloud_iter,cloud_iter + inliers.indices.back());
-
-			marker.lifetime = ros::Duration();
-			pub_marker.publish(marker);
-			pub_line_segment.publish(line_segment);
 		}
 
 		j++;
 	}
+
+	pub_line_segment.publish(points_msg);
 }
 
 void
@@ -186,25 +186,5 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input_point)
 
 	generate_lines_ransac(filtered_cloud);
 	pub_filtered_cloud.publish(filtered_cloud);
-}
-
-
-int
-main (int argc, char** argv)
-{
-	pcl::console::setVerbosityLevel(pcl::console::L_ALWAYS);
-
-	ros::init(argc,argv,"cloud_proc");
-	ros::NodeHandle nh;
-
-	ros::Subscriber sub_point = nh.subscribe ("pointcloud", 1, cloud_cb);
-
-	pub_filtered_cloud = nh.advertise<sensor_msgs::PointCloud2> ("filtered_pointcloud",1);
-	pub_marker = nh.advertise<visualization_msgs::Marker> ("line_strip",1);
-	pub_line_segment = nh.advertise<std_msgs::Float64MultiArray> ("line_segment",1);
-
-	ros::spin();
-
-	return 0;
 }
 
