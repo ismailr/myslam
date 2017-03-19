0d72b649f7c5bf33c1588a8da0a4f25b722ef368
Ismail <ismail@tass.telkomuniversity.ac.id>
2017-03-19 22:16:35 +0700

    rewrite

---
+++ layout_prediction/include/layout_prediction/layout_prediction.h
@@ -0,0 +1,20 @@
+#ifndef __LAYOUT_PREDICTION_H__
+#define __LAYOUT_PREDICTION_H__
+
+#include <geometry_msgs/Point.h>
+#include <pcl/point_cloud.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include "layout_prediction/helpers.h"
+
+typedef pcl::PointCloud<pcl::PointXYZ> pcloud;
+typedef pcl::PointCloud<pcl::Normal> pnormal;
+
+void cloud_cb2 (const sensor_msgs::PointCloud2::Ptr&);
+void reduce_pcl (const pcloud::Ptr&, const pcloud::Ptr&);
+void filter_pcl (const pcloud::Ptr&, const pcloud::Ptr&);
+void line_fitting (const pcloud::Ptr&, std::vector<geometry_msgs::PointStamped>&);
+void correct_pcl (const pcloud::Ptr&, const pcloud::Ptr&, const pcloud::Ptr&);
+void get_planes (const pcloud::Ptr&, std::vector<pcl::ModelCoefficients>, std::vector<pcl::PointIndices>);
+
+#endif
